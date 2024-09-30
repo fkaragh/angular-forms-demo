e2893e0da07cf94662d9b7e4af36132685e50cbd
fkaragh <fkfurkankara@gmail.com>
2024-09-30 17:34:32 +0300

    the form converted to the reactive form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,54 +1,68 @@
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+///////////////////////// TEMPLATE-DRIVEN FORM APPROACH ///////////////////////////////////
+
+// import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+// import { FormsModule, NgForm } from '@angular/forms';
+// import { debounceTime } from 'rxjs';
+
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [FormsModule],
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.css',
+// })
+// export class LoginComponent {
+//   private form = viewChild.required<NgForm>('form');
+//   private destroyRef = inject(DestroyRef);
+
+//   constructor(){
+//     afterNextRender(() => {
+//       const savedForm =  window.localStorage.getItem('saved-login-form');
+
+//       if (savedForm){
+//         const loadedFormData = JSON.parse(savedForm);
+//         const savedEmail = loadedFormData.email;
+//         setTimeout(() => { //to wait for the form to be initialized
+//           // this.form().setValue({
+//           //   email: savedEmail,
+//           //   password: '',
+//           // });
+//           this.form().controls['email'].setValue(savedEmail);
+//         }, 1);
+        
+//       }
+
+//       const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+//         next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}))
+//       });
+
+//       this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+
+//     });
+//   }
+
+//   onSubmit(formData: NgForm){
+//     if(formData.form.invalid){
+//       return;
+//     }
+
+//     const enteredEmail = formData.form.value.email;
+//     const enteredPassword = formData.form.value.password;
+
+//     console.log(enteredEmail, enteredPassword);
+
+//     formData.form.reset();
+//   }
+// }
+
+///////////////////////// END OF TEMPLATE-DRIVEN FORM APPROACH ///////////////////////////////////
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
-  private destroyRef = inject(DestroyRef);
-
-  constructor(){
-    afterNextRender(() => {
-      const savedForm =  window.localStorage.getItem('saved-login-form');
-
-      if (savedForm){
-        const loadedFormData = JSON.parse(savedForm);
-        const savedEmail = loadedFormData.email;
-        setTimeout(() => { //to wait for the form to be initialized
-          // this.form().setValue({
-          //   email: savedEmail,
-          //   password: '',
-          // });
-          this.form().controls['email'].setValue(savedEmail);
-        }, 1);
-        
-      }
-
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}))
-      });
-
-      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
-
-    });
-  }
-
-  onSubmit(formData: NgForm){
-    if(formData.form.invalid){
-      return;
-    }
-
-    const enteredEmail = formData.form.value.email;
-    const enteredPassword = formData.form.value.password;
-
-    console.log(enteredEmail, enteredPassword);
-
-    formData.form.reset();
-  }
-}
+export class LoginComponent {}
